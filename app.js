56fcc0f9bcd7dcde1eea9cbff18c85a6879640b1
SanjayaSamudra <sanjayasamudraelpitiya@gmail.com>
2024-06-25 22:32:16 +0530

    third commit

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
-let name = "saman";
-let age = 12;
-let isActive = false;
-let address = undefined;
+// let name = "saman";
+// let age = 12;
+// let isActive = false;
+// let address = undefined;
 
-console.log(typeof name);
-console.log(typeof age);
-console.log(typeof isActive);
-console.log(typeof address);
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isActive);
+// console.log(typeof address);
 
-//function initialization
-function sample() { 
-    //variable initialization of type var
-    var age = 12;
-    //by default word type is var if not defined
-    word = "name";
-    //variable initialization of type let
-    let x = "saman";
+// //function initialization
+// function sample() { 
+//     //variable initialization of type var
+//     var age = 12;
+//     //by default word type is var if not defined
+//     word = "name";
+//     //variable initialization of type let
+//     let x = "saman";
+// }
+
+// age = 20;
+// console.log(age);
+
+// age = 50;
+// console.log(age);
+
+//----------------------------JavaScript Objects---------------------------
+
+//object initialization
+const custom = {} 
+// 1. customer is the object reference
+// 2. Within the curly brackets{} object is found
+
+//object value initialization
+const customer = {
+    name: "saman" ,
+    age: 20 ,
+    address: "colombo" ,
+    phone: "0712345678" ,
+    email: "saman.com"
 }
-
-age = 20;
-console.log(age);
-
-age = 50;
-console.log(age);
+// 1. name, age, address, phone, email are attributes
+// 2. for each attribute values are assign using colon(:)
+// 3. all are let type attributes
+// 4. every single value are seperated from a comma(,)
+// 5. it is okay to have a comma at the end last item
+//      ex:- email: "saman.com",
+// 6. this object is more similar to appear like below
+//      ex:- const customer = {name: "saman", age: 20, address: "colombo", phone: "0712345678", email: "saman.com",}
