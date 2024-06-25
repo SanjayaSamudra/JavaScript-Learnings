91f5cf2c176cfc70e9d66bafe8a682c90d25904f
SanjayaSamudra <sanjayasamudraelpitiya@gmail.com>
2024-06-25 22:09:19 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,25 @@
+let name = "saman";
+let age = 12;
+let isActive = false;
+let address = undefined;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isActive);
+console.log(typeof address);
+
+//function initialization
+function sample() { 
+    //variable initialization of type var
+    var age = 12;
+    //by default word type is var if not defined
+    word = "name";
+    //variable initialization of type let
+    let x = "saman";
+}
+
+age = 20;
+console.log(age);
+
+age = 50;
+console.log(age);
