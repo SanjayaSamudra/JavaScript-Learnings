832ac806348d64df9dfee6c8aca186750dae907d
SanjayaSamudra <sanjayasamudraelpitiya@gmail.com>
2024-06-26 20:45:09 +0530

    fifth commit

--- app.js
+++ app.js
@@ -49,15 +49,15 @@
 // 6. this object is more similar to appear like below
 //      ex:- const customer = {name: "saman", age: 20, address: "colombo", phone: "0712345678", email: "saman.com",}
 
-//----------------------JavaScript Arrays-----------------------------
+//-------------------------------JavaScript Arrays-----------------------------------------
 
 //array initialization
 const demo = [];
-// 1. customer is the array reference
+// 1. demo is the array reference
 // 2. within the square brackets[] array items are found
 
 //setting up items in an array
-const name = [
+const nameArray = [
     "Kamal",
     "Amal",
     "Bimal",
@@ -65,5 +65,47 @@ const name = [
 ];
 
 // 1. "Kamal", "Amal", "Bimal", "Nimal" are array items
-// 2. each array 
+// 2. each array items are indexing from 0 to arraylength-1
+//      ex:- 
+//          arraylength = 4
+//          "Kamal" <--- Appear in index 0
+//          "Nimal" <--- Appear in index 3
 
+//===============================================================================================================
+
+//--------------------Assigning items after the array initialization---------------------------
+
+const customer = []; // array initialization with zero items
+customer[0] = "Sumal";
+customer[1] = 12;
+customer[2] = "Colombo";
+
+// like this we can initialize items later
+
+//===============================================================================================================
+
+//------------------------Calling array items seperately------------------------------
+
+console.log(nameArray[0]); // prints "Kamal"
+// This line of code reads as,
+//          From the reference called "nameArray" grab the data/item in the index 0th position and paste it to console.
+
+console.log(nameArray[1]); // prints "Amal"
+console.log(nameArray[2]); // prints "Bimal"
+console.log(nameArray[3]); // prints "Nimal"
+
+//===============================================================================================================
+
+//-------------------------------Getting array items into variables--------------------------------
+
+let firstName = nameArray[0];
+// What happens here is, we are assigning 0th index value of the reference named "nameArray" to the variable called firstName
+
+//================================================================================================================
+
+//---------------------------------Changing the item value after initialization----------------------------
+
+customer[0] = "Nimal";
+nameArray[0] = "Kithmal";
+
+//================================================================================================================
